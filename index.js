9438339779b7e16a3cd448335b36e22cf5a3243f
Tw1xan <tw1xan@yandex.ru>
2022-12-13 09:07:31 +0300

    morning hour

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.log(3 + 1);
 
 console.log(-3); // => -3
+
+console.log(6 - -81);
+
+console.log(-10 - -90);
+
+console.log(6 - -81);
+
+console.log("\\ \\ \\\\ \\ '\"");
+
+console.log('"Khal Drogo\'s favorite word is "athjahakar""');
+
+console.log("a".length);
+
+console.log("Gregor Clegane\nDunsen\nPolliver\nChiswyck");
